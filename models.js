6f6e5b7d4d069989e09956af528601b91a79cb29
Erk L <ericklo1094011@gmail.com>
2019-02-25 17:33:28 -0500

    Population not working, debugging in progress

--- models.js
+++ models.js
@@ -11,20 +11,14 @@ var UserSchema = new Schema({
   firstName: { type: String, required: true }, //require makes it so that the fields can't be left blank
   lastName: {type: String, required: true},
   emailAddress: {type: String, required: true},
-  password: {type: String, required: true}      
+  password: {type: String, required: true},     
 });
-
-//create the user model using Mongoose's model()
-var User = mongoose.model("User", UserSchema);
-
-//export Course model, then require the Course model into the routes file
-module.exports.User = User;
 
 
 
 //***CREATE COURSE SCHEMA***
 var CourseSchema = new Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}, // (_id from the users collection) by using population
+  user: { type: Schema.Types.ObjectId, ref: 'User'}, // (_id from the users collection) by using population
   title: {type: String, required: true},
   description: {type: String, required: true},
   estimatedTime: String,
@@ -34,8 +28,12 @@ var CourseSchema = new Schema({
 //create the course model using using Mongoose's model()
 var Course = mongoose.model("Course", CourseSchema);
 
+//create the user model using Mongoose's model()
+var User = mongoose.model("User", UserSchema);
 
 
-//export Course model, then require the Course model into the routes file
-module.exports.Course = Course;
+//export Course and User model, then require them into the routes file
+module.exports = {
+  Course, User,
+}
 
